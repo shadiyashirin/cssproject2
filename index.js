0d40184dd93b249adfa743580a4d3531e4d0823d
shadiyashirin <vshadiyashirin2003@gmail.com>
2024-09-12 08:53:01 +0530

    abc

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFun(){
+    var x=document.getElementById("menulist");
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+    } else {
+        x.style.display ="none";
+    }
+}
+
+
