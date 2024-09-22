f6304c7a04639897ae09e71e1da6a57211dbd47e
shadiyashirin <vshadiyashirin2003@gmail.com>
2024-09-22 20:30:34 +0530

    abh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function myFun(){
     var x=document.getElementById("menulist");
     if (x.style.display === "none")
     {
-        x.style.display = "inline";
+        x.style.display = "block";
     } else {
         x.style.display ="none";
     }
